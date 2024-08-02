8db1c83d3bae6f5e8c236d7f7367d40f2d2c3fed
etcshado <146455588+etcshado@users.noreply.github.com>
2024-08-02 09:27:04 +0200

    Update importantStuff.sh

--- BashScripts/importantStuff.sh
+++ BashScripts/importantStuff.sh
@@ -3,9 +3,9 @@
 # ls -S is size of files big -> small
 # ls -r is size of files small -> big (reversed)
 # $( commands | commands ) saves the output of a command
-# [ "string" = "kutas" ] check if string is equal to kutas
+# [ "string" = "example" ] check if string is equal to example
 # $(( 1+2 )) is arithmetic for saving the output of (( 1+2 ))
-# expr length "kutas" checks for lenght of the string
+# expr length "example" checks for lenght of the string
 # uniq -c counts number of occurances
 # uniq -u prints only unique lines
 # uniq -d prints only those who repeated
